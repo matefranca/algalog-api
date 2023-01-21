90bac1bf411ee69f59463fb8aaf7ccc9533778a3
Matheus Waked Franca <matheus363@hotmail.com>
2023-01-21 20:55:31 -0300

    Segunda aula.

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,31 +1,85 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+import com.algaworks.algalog.domain.service.CatalogoClienteService;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-
-	@GetMapping("/clientes")
+	
+	private ClienteRepository clienteRepository;
+	private CatalogoClienteService catalogoClienteService;
+	
+	@GetMapping
 	public List<Cliente> listar() {
-		Cliente cliente1 =new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Joao");
-		cliente1.setTelefone("21 99999-8888");
-		cliente1.setEmail("joaodascouves@algaworks.com");
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+				//.map(cliente -> ResponseEntity.ok(cliente))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 		
-		var cliente2 =new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("21 99999-8888");
-		cliente2.setEmail("mariadasilva@algaworks.com");
+//		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+//		
+//		if (cliente.isPresent()) {
+//			return ResponseEntity.ok(cliente.get());
+//		}
+//		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		return catalogoClienteService.salvar(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, 
+			@Valid @RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
 		
-		return Arrays.asList(cliente1,cliente2);
+		cliente.setId(clienteId);
+
+		cliente = catalogoClienteService.salvar(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		catalogoClienteService.excluir(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
